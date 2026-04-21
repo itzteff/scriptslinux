a38ee579878141df5573eb2bfad1403b7ac6cb5f
itzteff <166640043+itzteff@users.noreply.github.com>
2026-04-21 13:29:00 +0300

    Update zapret.sh

--- zapret/zapret.sh
+++ zapret/zapret.sh
@@ -3,3 +3,6 @@ sudo pacman -Sy git nftables curl --needed --noconfirm
 git clone https://github.com/Sergeydigl3/zapret-discord-youtube-linux.git ~/zapret-discord-youtube-linux/
 cd ~/zapret-discord-youtube-linux
 ./service.sh download-deps --default
+sudo rm -rf custom-strategies
+mkdir custom-strategies
+mv ~/scriptslinux/custom-strategies/general_SIMPLE_FLOWSEAL_FAKE.bat ~/zapret-discord-youtube-linux/custom-strategies
