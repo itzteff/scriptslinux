f9f1ee36f5e0375ba2aaa93b5107ad4a00d92c18
itzteff <166640043+itzteff@users.noreply.github.com>
2026-04-21 13:44:43 +0300

    Update zapret.sh

--- zapret/zapret.sh
+++ zapret/zapret.sh
@@ -2,6 +2,7 @@
 sudo pacman -Sy git nftables curl xdotool --needed --noconfirm
 git clone https://github.com/Sergeydigl3/zapret-discord-youtube-linux.git ~/zapret-discord-youtube-linux/
 cd ~/zapret-discord-youtube-linux
+chmod +x service.sh
 ./service.sh download-deps --default
 sudo rm -rf custom-strategies
 mkdir custom-strategies
@@ -9,3 +10,7 @@ cp ~/scriptslinux/zapret/custom-strategies/general_SIMPLE_FLOWSEAL_FAKE.bat ~/za
 sudo rm -rf ~/zapret-discord-youtube-linux/zapret-latest/lists/list-general.txt
 cp ~/scriptslinux/zapret/lists/list-general.txt ~/zapret-discord-youtube-linux/zapret-latest/lists
 cp ~/scriptslinux/zapret/conf.env ~/zapret-discord-youtube-linux/
+cd ~/zapret-discord-youtube-linux
+./service.sh
+sleep 2
+xdotool key 1
