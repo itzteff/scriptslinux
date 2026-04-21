69cd18444a7ec0b2a6517c6a97a4fb13f09f82ed
itzteff <166640043+itzteff@users.noreply.github.com>
2026-04-21 13:23:31 +0300

    Update zapret.sh

--- zapret/zapret.sh
+++ zapret/zapret.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+sudo pacman -Sy git nftables curl --needed --noconfirm
+git clone https://github.com/Sergeydigl3/zapret-discord-youtube-linux.git ~/zapret-discord-youtube-linux/
+cd ~/zapret-discord-youtube-linux
+./service.sh download-deps --default
